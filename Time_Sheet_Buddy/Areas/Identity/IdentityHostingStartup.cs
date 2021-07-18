3794a38832c550b0f805c801c703a9f6124dfc5d
kostadinmarkov99 <kostadin.markov99@gmail.com>
2021-07-18 20:00:41 +0300

    Add all Identity functionality for the project

---
+++ Time_Sheet_Buddy/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Time_Sheet_Buddy.Data;
+
+[assembly: HostingStartup(typeof(Time_Sheet_Buddy.Areas.Identity.IdentityHostingStartup))]
+namespace Time_Sheet_Buddy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Time_Sheet_Buddy/Data/ContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Time_Sheet_Buddy.Models;
+
+namespace Time_Sheet_Buddy.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedSuperAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@gmail.com",
+                FirstName = "Kostadin",
+                LastName = "Markov",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word.");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Basic.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Administrator.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.SuperAdministrator.ToString());
+                }
+
+            }
+        }
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Administrator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.SuperAdministrator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Basic.ToString()));
+        }
+    }
+}
