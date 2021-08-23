9e08c002732833ecb0b792c7dbb4f40b15b9fc19
kostadinmarkov99 <kostadin.markov99@gmail.com>
2021-08-24 00:08:08 +0300

    Add Idea Functionalities and Views

---
+++ Time_Sheet_Buddy/Models/IdeaPropModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Time_Sheet_Buddy.Models
+{
+    public class IdeaPropModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
